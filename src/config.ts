9c091cec8870ce07e5aa3728515fbecd42dde462
Varun <devvarunbhardwaj@gmail.com>
2024-10-14 15:11:02 +0530

    ::

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,1 @@
 export const NAME = " Travell Tales ";
-export const HEADING = " This will be the Heading ";
-export const SUB_HEADING = "This will be subHeading ";
-export const SMALL_HEADING = " ✨ this will be the small heading ";
-export const SMALL_HEADING_LINK = "/"
--- src/providers.tsx
+++ src/providers.tsx
@@ -6,7 +6,7 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
     >
